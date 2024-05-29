9a853b34373f204315753ed942cd527ef71e7baa
Meshwil Anthony Dias <meshwildias@gmail.com>
2024-05-29 22:12:55 +0100

    Added LSP stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 -- telescope plugin
 return {
+  {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
@@ -8,5 +9,20 @@ return {
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end 
+  end
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
