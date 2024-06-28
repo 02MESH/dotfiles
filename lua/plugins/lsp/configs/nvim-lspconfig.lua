06df91f98a54ba777566b6bd5728c0ea84d573a4
Meshwil Anthony Dias <meshwildias@gmail.com>
2024-06-28 23:54:11 +0100

    Latest latex changes with code refactoring for easier reading.

---
+++ lua/plugins/lsp/configs/nvim-lspconfig.lua
@@ -0,0 +1,25 @@
+return {
+	"neovim/nvim-lspconfig",
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.tsserver.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.vimls.setup({
+			capabilities = capabilities,
+		})
+
+		-- Keymaps for LSP functions
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+	end,
+}
